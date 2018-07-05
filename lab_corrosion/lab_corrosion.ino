b26bfba382bb196f3d18dbac893b01d152b01b64
andrew_collison <andrew.collison.ac@gmail.com>
2018-07-05 15:31:57 +1000

    added the code description and arduino sketch files

---
+++ lab_corrosion/lab_corrosion.ino
@@ -0,0 +1,18 @@
+// Institution: Queensland University of Technology
+// Author: Andrew Collison
+// Supervisour: Assoc Prof. Geoffery Will
+/* The driving code for an arduino motor driver and data logger
+ *  used to raise and lower corrosion samples out of a solution
+ *  to control and monitor the corrosion rate of various samples
+ *  in a lab based environment
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
